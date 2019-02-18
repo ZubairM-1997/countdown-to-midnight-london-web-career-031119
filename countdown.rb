36913dbe63d024a49c1109c281a02307de8867c4
placid-computer-9089 <zubairmaqsood866@gmail.com>
2019-02-18 02:56:30 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -9,4 +9,13 @@ x = number
 puts "HAPPY NEW YEAR!"
 end 
   
-def countdown_with_sleep(number)
+def countdown_with_sleep(seconds_to_midnight)
+x = seconds_to_midnight
+  while x > 0
+    puts "#{x} SECOND(S)!"
+    sleep(1)
+    x -= 1
+  end
+puts "HAPPY NEW YEAR!"
+end
+  
