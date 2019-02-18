63f12a1a89631bd85e5a83e1762e43b5bacea7ec
placid-computer-9089 <zubairmaqsood866@gmail.com>
2019-02-18 02:49:29 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,12 @@
 #write your code here
 
 def countdown(number)
-  while number > 0 
-    puts "#{number} SECONDS!"
-    number -= 1 
-  break if number == 0 
-    puts "HAPPY NEW YEAR!"
-  end 
-end
+x = number 
+  while x > 0 
+    puts "#{x} SECONDS!"
+    x -= 1
+    break if x == 0 
+      puts "HAPPY NEW YEAR"
+    end 
+end 
+  
