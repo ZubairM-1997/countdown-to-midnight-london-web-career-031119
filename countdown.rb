eb544cd3cd433bff88640bdf0879c202f0179772
placid-computer-9089 <zubairmaqsood866@gmail.com>
2019-02-18 02:57:42 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,21 @@
 #write your code here
 
-def countdown
+def countdown(number)
+x = number 
+  while x > 0 
+    puts "#{x} SECONDS!"
+    x -= 1
+   end 
+puts "HAPPY NEW YEAR!"
+end 
+  
+def countdown_with_sleep(seconds_to_midnight)
+x = seconds_to_midnight
+  while x > 0
+    puts "#{x} SECOND(S)!"
+    sleep(1)
+    x -= 1
+  end
+puts "HAPPY NEW YEAR!"
 end
+  
